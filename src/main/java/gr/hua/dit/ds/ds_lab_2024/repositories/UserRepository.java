11187edd77eb1897f3d706bd0d7023ef0d53e525
Spyros_Gew <spyrosgewdark@gmail.com>
2024-12-10 20:36:10 +0200

    Delete src/main/java/gr/hua/dit/ds/ds_lab_2024/repositories/UserRepository.java

--- src/main/java/gr/hua/dit/ds/ds_lab_2024/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package gr.hua.dit.ds.ds_lab_2024.repositories;
-
-import gr.hua.dit.ds.ds_lab_2024.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
