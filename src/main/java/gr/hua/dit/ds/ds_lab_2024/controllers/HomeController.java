57cd706d362d6d995705bec889831e8fc2d556ea
SpyrosGew <spyrosgewdark@gmail.com>
2025-01-31 23:34:22 +0200

    started ui so cutsy

--- src/main/java/gr/hua/dit/ds/ds_lab_2024/controllers/HomeController.java
+++ src/main/java/gr/hua/dit/ds/ds_lab_2024/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package gr.hua.dit.ds.ds_lab_2024.controllers;
 
+import gr.hua.dit.ds.ds_lab_2024.service.PropertyService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,11 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    private PropertyService propertyService;
 
+    public HomeController(PropertyService propertyService){
+        this.propertyService = propertyService;
+    }
     @Operation(summary = "Home page", description = "Returns the home page of the application.")
     @GetMapping
     public String home(Model model) {
         model.addAttribute("title", "Home");
+        model.addAttribute("properties", propertyService.getProperties());
         return "index"; // Main page
     }
 }
